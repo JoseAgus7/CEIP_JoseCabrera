36f6893b0f716f98edcfa0fd269e3506433294d3
Hugo Luna Muñoz <hugolunmu@gmail.com>
2026-05-20 13:37:50 +0200

    feat: añadir componente CardHugo

---
+++ components/cards/CardHugo.jsx
@@ -0,0 +1,17 @@
+export default function CardEjemplo() {
+  return (
+    <div className="tarjeta">
+      <span className="avatar">🧑‍🏫</span>
+      <h3>Hugo Luna</h3>
+      <p className="rol">Estudiante 1DAW</p>
+      <p className="bio">
+        Tengo 19 años y me gusta el deporte
+      </p>
+      <div className="tags">
+        <span>Git</span>
+        <span>GitHub</span>
+        <span>Next.js</span>
+      </div>
+    </div>
+  );
+}
