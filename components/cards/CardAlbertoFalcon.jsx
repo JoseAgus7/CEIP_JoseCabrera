863174a44ed22ee00484cd73b49b1f3597282bad
Alberto Falcon Cordero <albertofalconcordero@gmail.com>
2026-05-21 09:44:50 +0200

    feat: Añadir componente CardAlbertoFalcon.jsx

---
+++ components/cards/CardAlbertoFalcon.jsx
@@ -0,0 +1,17 @@
+export default function CardEjemplo() {
+  return (
+    <div className="tarjeta">
+      <span className="avatar">🧑‍🏫</span>
+      <h3>Alberto Falcon Cordero</h3>
+      <p className="rol">Instructor del taller</p>
+      <p className="bio">
+        Soy Alberto Falcon Cordero estudiante de 1ºDaw de Ceip Jose Cabrera
+      </p>
+      <div className="tags">
+        <span>HTML</span>
+        <span>CSS</span>
+        <span>JavaScript</span>
+      </div>
+    </div>
+  );
+}
