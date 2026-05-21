ecd8abe68ea6db17c8896f0002b26da18c378592
Manuel David Benitez Tejero <beniteztejerom@gmail.com>
2026-05-21 10:32:40 +0200

    añadir componente CardManuel

---
+++ components/cards/CardManuel.jsx
@@ -0,0 +1,17 @@
+export default function CardEjemplo() {
+  return (
+    <div className="tarjeta">
+      <span className="avatar">🧑‍💻</span>
+      <h3>Manuel</h3>
+      <p className="rol">Desarrollador WEB</p>
+      <p className="bio">
+        Divide y vencerás
+      </p>
+      <div className="tags">
+        <span>HTML</span>
+        <span>CSS</span>
+        <span>JavaScript</span>
+      </div>
+    </div>
+  );
+}
